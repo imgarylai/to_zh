3721727fd9ef5b072193ea32e4a39bfd8d5fd3d8
Gary Lai <gary@gary-lai.com>
2016-03-25 16:13:53 +0800

    I have implemented upper case so far.

--- lib/to_zh.rb
+++ lib/to_zh.rb
@@ -1,4 +1,4 @@
-require "to_zh/version"
+require 'to_zh/version'
 
 module ToZh
   module InstanceMethods
@@ -6,13 +6,41 @@ module ToZh
       ToZh.to_zh(self)
     end
   end
+  UPPPER_DIGIT = ['零', '壹', '貳', '參', '肆', '伍', '陸', '柒', '捌', '玖']
+  UPPER_DECIMAL_UNIT = ['', '拾', '佰', '仟']
+  LOWER_DIGIT = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+  LOWER_DECIMAL_UNIT = ['', '十', '百', '千']
+  THOUSAND_UNIT = ['', '萬', '億', '兆', '京', '垓', '秭', '穰', '溝', '澗', '正', '載', '極']
+  UNIT_LENGTH = 4
 
   def self.to_zh int
-    upper_zh_num = ['零', '壹', '貳', '參', '肆', '伍', '陸', '柒', '捌', '玖']
-    decimal_unit = ['', '拾', '佰', '仟']
-    thousand_unit = ['', '萬', '億', '兆', '京', '垓', '秭', '穰', '溝', '澗', '正', '載', '極']
-    # ex: convert 100 -> 壹佰, 123,456,789 -> 壹億貳仟參佰肆拾伍萬陸仟柒佰捌拾玖
+    negative = (int < 0)
+    return UPPPER_DIGIT[int] if int == 0
+    arr = int.abs.to_s.chars.map(&:to_i).reverse.each_slice(4).map(&:reverse).reverse
+    result = []
+    need_zero_offset = true
+    arr.each_with_index do |a, index|
+      if a.inject(:+) == 0 && need_zero_offset && arr.last.inject(:+) != 0
+        result.push(UPPPER_DIGIT[0])
+        need_zero_offset = false
+      elsif a.inject(:+) != 0
+        a.each_with_index do |i, index|
+          if i == 0 && index == 0 && need_zero_offset
+            result.push(UPPPER_DIGIT[i])
+          elsif i == 0 && index != 0
+            next
+          elsif i != 0
+            result.push(UPPPER_DIGIT[i])
+            result.push(UPPER_DECIMAL_UNIT[a.length - index - 1])
+          end
+        end
+        result.push(THOUSAND_UNIT[(int.to_s.length/UNIT_LENGTH.to_f).ceil - index - 1])
+        need_zero_offset = true
+      end
+    end
+    result.join
   end
+
 end
 
-Integer.send :include, ToZh::InstanceMethods
+Integer.send(:include, ToZh::InstanceMethods)
--- spec/to_zh_spec.rb
+++ spec/to_zh_spec.rb
@@ -5,7 +5,36 @@ describe ToZh do
     expect(ToZh::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'should get zero' do
+    expect(0.to_zh).to eq "零"
+    expect(0000.to_zh).to eq "零"
   end
+
+  it 'should get upper zh chars in length smaller than 4' do
+    expect(123.to_zh).to eq "壹佰貳拾參"
+    expect(456.to_zh).to eq "肆佰伍拾陸"
+  end
+
+  it 'should get upper zh chars in length larger than 5' do
+    expect(12345.to_zh).to eq "壹萬貳仟參佰肆拾伍"
+    expect(102345.to_zh).to eq "壹拾萬貳仟參佰肆拾伍"
+  end
+
+  it 'should get upper zh chars with zero offset' do
+    expect(100002345.to_zh).to eq "壹億零貳仟參佰肆拾伍"
+    expect(1000000002345.to_zh).to eq "壹兆零貳仟參佰肆拾伍"
+  end
+
+  it 'should get upper zh chars end with zero' do
+    expect(100.to_zh).to eq "壹佰"
+    expect(10000.to_zh).to eq "壹萬"
+    expect(100000000.to_zh).to eq "壹億"
+    expect(120.to_zh).to eq "壹佰貳拾"
+    expect(10000102340.to_zh).to eq "壹佰億零壹拾萬貳仟參佰肆拾"
+  end
+
+  it 'should get upper zh chars with zero offset and end with zero' do
+    expect(1000000000010.to_zh).to eq "壹兆零壹拾"
+  end
+
 end
